c52c8da806639f5a8c38b92cea5f8b0f768b8095
acilegna <acilegna.airam88@gmail.com>
2025-09-12 23:20:42 -0600

    cambio  url

--- src/services/datosInvitados.js
+++ src/services/datosInvitados.js
@@ -6,7 +6,9 @@ import { Children } from "react";
 //creando un objeto API que tiene varias funciones para hacer peticiones.
 //export const confirmed = () => getData("http://127.0.0.1:8000/api/asistiran");
 //const api = "http://127.0.0.1:8000/api/";
- const api = " http://apiexcel.ladoobscurodelaluna.com/public/api/";
+ 
+ const api = "https://backapi.ladoobscurodelaluna.com/public/api/";
+
 
 export const Invitados = {
   // Usas dos puntos : para decir “esta propiedad-funcion (confirmed)   tiene este valor getData”.
--- src/services/importFile.js
+++ src/services/importFile.js
@@ -3,7 +3,7 @@ export async function importFile(file) {
   const formData = new FormData();
   formData.append("file", file);
 //  const api = "http://127.0.0.1:8000/api/";
- const api = " http://apiexcel.ladoobscurodelaluna.com/public/api/";
+ const api = "https://backapi.ladoobscurodelaluna.com/public/api/";
  
 
   try {
