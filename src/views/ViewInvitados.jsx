b9236a180d3b34cf319d9dfbb950d43f3d3c5a51
acilegna <acilegna.airam88@gmail.com>
2025-11-08 00:21:32 -0600

    TABLE

--- src/views/ViewInvitados.jsx
+++ src/views/ViewInvitados.jsx
@@ -1,116 +1,118 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 
+import DataTable from 'react-data-table-component';
 //recibe los datos q envia la vista viewPanel (componente principal-padre)
 const ViewInvitados = ({ data, errores, message, processo, changeTitle, Confirmation, handleInputChange, inputValue }) => {
     const process = "delete";
 
-    return (
-        <div className="table-responsive">
-            <table className="table table-striped table-bordered align-middle">
-                <thead>
-                    <tr>
-                        <th colSpan="8">
-                            <div className="row g-2">
-                                <div className="col">
-                                    <input
-                                        id="filter"
-                                        type="search"
-                                        className="form-control"
-                                        placeholder="Buscar"
-                                        onChange={handleInputChange}
-                                        value={inputValue}
-                                    />
-                                </div>
-                                <div className="col-auto">
-                                    <Link className="btn btn-primary" to="/editar" onClick={() => changeTitle()}>
-                                        <i className="bi bi-person-add text-white"> Agregar Invitado</i>
-                                    </Link>
-                                </div>
-                            </div>
-                        </th>
-                    </tr>
-                    <tr>
-                        <th>Codigo</th>
-                        <th>Nombre</th>
-                        <th>Apellido</th>
-                        {/*  <th>Cel</th> */}
-                        <th>Grupo</th>
-                        <th>Status</th>
-                        <th>Asistira</th>
-                        <th>Acciones</th>
+    const columns = [
+          {
+            name: 'Codigo',
+            selector: row => row.id_familia,
+        },
+        {
+            name: 'Nombre',
+            selector: row => row.name,
+        },
+        {
+            name: 'Apellido',
+            selector: row => row.apellido,
+        },
+        {
+            name: 'Grupo',
+            selector: row => row.categoria,
+        },
+        {
+            name: 'Status',
+            selector: row => row.status,
+            cell: row => (
+                <div
+                    style={{
+                        backgroundColor:
+                            row.status === "Pendiente"
+                                ? "#fcf186"   // Amarillo
+                                : row.status === "No"
+                                    ? "#fc8686"   // Rojo
+                                    : row.status === "Sí"
+                                        ? "#47de81"   // Verde
+                                        : "#cccccc",  // Gris por defecto
+                        borderRadius: "8px",
+                        padding: "4px 8px",
+                        textAlign: "center",
+                        fontWeight: "bold",
+                        minWidth: "80px",
+                    }}
+                >
+                    {row.status}
+                </div>
+            ),
+        },
+        {
+            name: 'Asistira',
+            selector: row => row.id,
+            cell: row => (<div>
+                <Link to="/invitados" onClick={() => Confirmation(row.id, { status: "Si" })}>
+                    <i className="bi bi-check-circle" title="Sí asistirá"></i>
+                </Link>
 
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((invitado) => (
-                        <tr key={invitado.id}>
-                            <td>{invitado.id_familia}</td>
-                            <td>{invitado.name}</td>
-                            <td>{invitado.apellido}</td>
-                            {/* <td>{invitado.telefono}</td> */}
-                            <td>{invitado.categoria}</td>
-                            <td
-                                style={{
-                                    backgroundColor:
-                                        invitado.status === "Pendiente"
-                                            ? " #fcf186"
-                                            : invitado.status === "No"
-                                                ? "#fc8686"
-                                                : invitado.status === "Sí"
-                                                    ? "#47de81"
-                                                    : "#47de81",
-                                }}
-                            >
-                                {invitado.status}
-                            </td>
+                <Link to="/invitados" onClick={() => Confirmation(row.id, { status: "No" })}>
+                    <i className="bi bi-x-circle" title="No asistira"></i>
+                </Link>
+                <Link to="/invitados" onClick={() => Confirmation(row.id, { status: "Pendiente" })}>
+                    <i className="bi bi-hourglass-split" title="Pendiente por confirmar"></i>
+                </Link>
+            </div>
+            )
 
-                            <td>
-
-                                <Link to="/invitados" onClick={() => Confirmation(invitado.id, { status: "Si" })}>
-                                    <i className="bi bi-check-circle" title="Sí asistirá"></i>
-                                </Link>
-
-                                <Link to="/invitados" onClick={() => Confirmation(invitado.id, { status: "No" })}>
-                                    <i className="bi bi-x-circle" title="No asistira"></i>
-                                </Link>
-                                <Link to="/invitados" onClick={() => Confirmation(invitado.id, { status: "Pendiente" })}>
-                                    <i className="bi bi-hourglass-split" title="Pendiente por confirmar"></i>
-                                </Link>
-                            </td>
-                            <td className="d-flex gap-2 justify-content-center">
-                                <Link to="/invitados" onClick={() => processo(invitado.id, process)}>
+        },
+        {
+            name: 'Acciones',
+            selector: row => row.id,
+             cell: row => (<div>
+                 <Link to="/invitados" onClick={() => processo(row.id, process)}>
                                     <i className="bi bi-trash text-danger" title="Eliminar"></i>
                                 </Link>
-                                <Link to="/editar" onClick={() => processo(invitado.id)}>
+                                <Link to="/editar" onClick={() => processo(row.id)}>
                                     <i className="bi bi-pencil-fill text-primary" title="Modificar"></i>
                                 </Link>
-                            </td>
 
-                        </tr>
-                    ))}
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <td colSpan="8">
-                            {/*  si message tiene valor entonces renderiza */}
-                            {/*   {message && (
-                                <h5 className="alert alert-danger mt-3">
-                                    {message}
-                                </h5>
-                            )} */}
-                            {/*   {errores && (
-                                <h5 className="alert alert-danger mt-3">
-                                    {errores}
-                                </h5>
-                            )} */}
-                        </td>
-                    </tr>
-                </tfoot>
-            </table>
+            </div>
+            )
+        },
+    ];
+
+  return (
+    <div className="container mt-3">
+        <div className="row mb-3">
+            <div className="col">
+                <input
+                    id="filter"
+                    type="search"
+                    className="form-control"
+                    placeholder="Buscar"
+                    onChange={handleInputChange}
+                    value={inputValue}
+                />
+            </div>
+            <div className="col-auto">
+                <Link
+                    className="btn btn-primary"
+                    to="/editar"
+                    onClick={() => changeTitle()}
+                >
+                    <i className="bi bi-person-add text-white"> Agregar Invitado</i>
+                </Link>
+            </div>
         </div>
 
-    )
+        <DataTable
+            columns={columns}
+            data={data}
+        />
+    </div>
+);
+
 }
 //exportamos para llamar a la vista desde el componente padre
 export default ViewInvitados
