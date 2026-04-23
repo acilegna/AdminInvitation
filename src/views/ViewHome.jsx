a48f34b898d4b1e7e3865f87071cb45585586c72
angelica <acilegna.airam88@gmail.com>
2026-04-23 15:06:14 -0600

    pruebs

--- src/views/ViewHome.jsx
+++ src/views/ViewHome.jsx
@@ -2,6 +2,7 @@ const ViewHome = () => {
   return (
     <>
       <div className="container-fluid">
+        hjhj
         {" "}
         <div className="cont-details p-3 text-white rounded d-flex flex-column justify-content-center align-items-center">
           <img src="/sources/IMG_1150.jpg" alt="Fondo" className="imagenes2" />
