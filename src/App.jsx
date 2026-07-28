058009c18ec3f29541c5fc08450d5c08626d6e64
angelica <acilegna.airam88@gmail.com>
2026-07-27 18:44:33 -0600

    audio automatico

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import './invitation.css'
-import ViewPanel from './views/ViewPanel';
-import ViewInvitation from './views/ViewInvitation';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./invitation.css";
+import ViewPanel from "./views/ViewPanel";
+import ViewInvitation from "./views/ViewInvitation";
 
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/*" element={<ViewPanel />} />
           <Route path="/bya" element={<ViewInvitation />} />
+          {/*
+           */}{" "}
           {/*   <Route path="/*" element={<Back />} />     */}
-
         </Routes>
       </BrowserRouter>
     </>
