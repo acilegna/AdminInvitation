b1d11636f995e47df901d680914a04b3573c5732
angelica <acilegna.airam88@gmail.com>
2026-08-03 18:25:36 -0600

    animo

--- src/hooks/useScrollSections.js
+++ src/hooks/useScrollSections.js
@@ -39,6 +39,21 @@ export default function useScrollSections() {
       return () => animate(element, { opacity: 0, x: -100 });
     });
 
+     // animacion pra titulo
+    inView(".seccion a", (element) => {
+      animate(
+        element,
+        { opacity: 1, x: [-100, 0] },
+        {
+          duration: 0.9,
+          easing: [0.17, 0.55, 0.55, 1],
+        }
+      );
+
+      // cleanup: cuando sale de vista
+      return () => animate(element, { opacity: 0, x: -100 });
+    });
+
     inView(".seccion .izq", (element) => {
       animate(
         element,
