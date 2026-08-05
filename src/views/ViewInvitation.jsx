180c43d9cbd3c88ea306032d198c267d0266bcaa
angelica <acilegna.airam88@gmail.com>
2026-08-04 23:06:54 -0600

    aaaj

--- src/views/ViewInvitation.jsx
+++ src/views/ViewInvitation.jsx
@@ -102,7 +102,7 @@ function Content({ toggleAudio, reproduciendo }) {
         <div className="cont-full seccion ">
           <div className="imagenes-container downup">
             <img
-              src="/sources/pardeja.jpg"
+              src="/sources/1.jpg"
               alt="Fondo"
               className="img-seccion-one scale"
             />
